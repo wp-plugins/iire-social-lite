e6626a8f3be3340c8f8b2746a0c8367cd70ad991
srussell13 <srussell13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-15 21:31:19 +0000

    Backup 0.21
    git-svn-id: https://plugins.svn.wordpress.org/iire-social-lite/tags/0.21@639815 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin_iire_social_lite_hooks.php
@@ -0,0 +1,68 @@
+<?php
+// Admin Social Lite Hooks - 12-12-2012
+
+function iire_social_lite_admin_pages() {
+	add_menu_page('iiRe Social Lite', 'iiRe Social Lite ', 'administrator', 'iire_admin_social_lite_home', 'iire_admin_social_lite_home');
+	add_submenu_page('iire_admin_social_lite_home', 'Widget Settings', 'Widget Settings', 10, 'iire_admin_social_lite_widget', 'iire_admin_social_lite_widget');
+	add_submenu_page('iire_admin_social_lite_home', 'Shortcode Settings', 'Shortcode Settings', 10, 'iire_admin_social_lite_shortcode', 'iire_admin_social_lite_shortcode');			
+}
+add_action('admin_menu', 'iire_social_lite_admin_pages');
+
+
+function iire_social_lite_admin_enable_js() {
+    if (is_admin() && (($_GET['page'] == 'iire_admin_social_lite_widget') || ($_GET['page'] == 'iire_admin_social_lite_shortcode'))  ){
+		wp_enqueue_script( 'color-picker', IIRE_SOCIAL_LITE_URL.'includes/admin_colorpicker.js');
+	
+		wp_enqueue_script('jquery-ui-core');
+		wp_enqueue_script('jquery-ui-widget');
+		wp_enqueue_script('jquery-ui-mouse');	
+		wp_enqueue_script('jquery-ui-draggable');
+		wp_enqueue_script('jquery-ui-droppable');
+		wp_enqueue_script('jquery-ui-resizable');	
+		wp_enqueue_script('jquery-ui-selectable');
+		wp_enqueue_script('jquery-ui-sortable');	
+		wp_enqueue_script('jquery-ui-effect');	
+		wp_enqueue_script('jquery-ui-accordion');
+		wp_enqueue_script('jquery-ui-button');				
+		wp_enqueue_script('jquery-ui-dialog');
+		wp_enqueue_script('jquery-ui-position');		
+		wp_enqueue_script('jquery-ui-slider');		
+		wp_enqueue_script('jquery-ui-tabs');				
+    }
+	
+	 if (is_admin() && ($_GET['page'] == 'iire_admin_social_lite_widget')){
+		wp_enqueue_script( 'social-widget', IIRE_SOCIAL_LITE_URL.'includes/admin_iire_social_lite_widget.js');
+	}
+
+	 if (is_admin() && ($_GET['page'] == 'iire_admin_social_lite_shortcode')){
+		wp_enqueue_script( 'social-widget', IIRE_SOCIAL_LITE_URL.'includes/admin_iire_social_lite_shortcode.js');
+	}		
+}
+add_action('admin_print_scripts', 'iire_social_lite_admin_enable_js');
+
+
+function iire_social_lite_admin_enable_styles() {
+    if ( is_admin() && (($_GET['page'] == 'iire_admin_social_lite_widget') || ($_GET['page'] == 'iire_admin_social_lite_shortcode')) ){
+		if( !wp_script_is('jquery-ui_css') ) { 		
+			$x = explode('/',IIRE_SOCIAL_LITE_BASENAME);
+			$d = "../".IIRE_SOCIAL_LITE_CONTENT_URL."/plugins/".$x[0]."/includes/jquery-ui.css";	
+			if (file_exists($d)) {
+				wp_enqueue_style( 'jquery-ui_css', IIRE_SOCIAL_LITE_URL.'includes/jquery-ui.css');				
+			}
+		}			
+	}
+}
+add_action( 'admin_print_styles', 'iire_social_lite_admin_enable_styles' );
+
+
+function iire_social_lite_admin_register_head() {
+    if (is_admin() && (($_GET['page'] == 'iire_admin_social_lite_widget') || ($_GET['page'] == 'iire_admin_social_lite_shortcode') || ($_GET['page'] == 'iire_admin_social_lite_home'))){
+    	echo '<link rel="stylesheet" type="text/css" href="'.IIRE_SOCIAL_LITE_URL.'includes/admin_iire_social_lite_styles.css" />';
+	}
+
+    if (is_admin() && (($_GET['page'] == 'iire_admin_social_lite_widget') || ($_GET['page'] == 'iire_admin_social_lite_shortcode'))){
+    	echo '<link rel="stylesheet" type="text/css" href="'.IIRE_SOCIAL_LITE_URL.'includes/iire_social_lite_icons.css" />';
+    }			
+}
+add_action('admin_head', 'iire_social_lite_admin_register_head');
+?>
---
+++ includes/iire_social_lite_widget.php
@@ -0,0 +1,57 @@
+<?php
+// iiRe Social Lite Widget - 12-12-2012
+class iiReSocialLite extends WP_Widget {
+
+  	function iiReSocialLite()  {
+    	$widget_ops = array('classname' => 'iiReSocialLite', 'description' => 'Social Lite Icons for widget' );
+    	$this->WP_Widget('iiReSocialLite', 'iiRe Social Lite Icons', $widget_ops);
+  	}
+ 
+
+	function form($instance)  {
+    	$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    	$title = $instance['title'];
+		?>
+  		<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>		
+
+		<?php
+	}
+ 
+
+	function update($new_instance, $old_instance) {
+    	$instance = $old_instance;
+    	$instance['title'] = $new_instance['title'];
+    	return $instance;
+  	}
+ 
+
+  	function widget($args, $instance) {
+    	extract($args, EXTR_SKIP);
+
+    	$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+		
+        echo $before_widget;
+        if(!empty($title)) {
+           echo $before_title.$title.$after_title;
+        }
+		
+		global $wpdb;
+		global $blog_id;
+
+		$table_name = $wpdb->get_blog_prefix($blog_id)."iire_social_lite";
+	
+		$settings = array();		
+		$rs = $wpdb->get_results("SELECT * FROM $table_name");
+		foreach ($rs as $row) {
+			$settings[$row->option_name] = $row->option_value;
+		}		
+		
+		echo '<div id="iire_social_lite_widget" class="iire_social_lite_widget">';
+		echo stripslashes($settings['widget_output']);
+		echo '</div>';
+
+		echo $after_widget;
+  	}
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("iiReSocialLite");') );
+?>
